37e75303b0d8df69964ce9c233f8c968a0b8bb80
Shinsaku Hiura <86639425+sh1ura@users.noreply.github.com>
2023-01-09 18:47:11 +0900

    Add files via upload

---
+++ arduino/haptic-mouse.ino
@@ -0,0 +1,24 @@
+#define SOLENOID_PIN 2
+
+void setup() {
+  pinMode(SOLENOID_PIN, OUTPUT);
+  Serial.begin(115200);
+}
+
+long int ontime;
+
+void loop() {
+//  if(ontime + 1000 < millis()) {
+//    digitalWrite(SOLENOID_PIN, LOW);
+//  }
+  int val = Serial.read();
+  if(val < 0)
+    return;
+  if(val == '1') {
+    digitalWrite(SOLENOID_PIN, HIGH);
+    ontime = millis();
+  }
+  else if(val == '0') {
+    digitalWrite(SOLENOID_PIN, LOW);
+  }
+}
